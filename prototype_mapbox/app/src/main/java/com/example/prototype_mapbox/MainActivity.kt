a5831b9866f995ee014aab73b3b2d6475a8e5be2
garrett-low <usernamegl@live.com>
2024-05-12 22:26:50 -0500

    Create mapview without compose - no black screen

--- prototype_mapbox/app/src/main/java/com/example/prototype_mapbox/MainActivity.kt
+++ prototype_mapbox/app/src/main/java/com/example/prototype_mapbox/MainActivity.kt
@@ -2,41 +2,55 @@ package com.example.prototype_mapbox
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.ui.Modifier
 import com.mapbox.common.MapboxOptions
 import com.mapbox.geojson.Point
-import com.mapbox.maps.extension.compose.MapboxMap
-import com.mapbox.maps.extension.compose.animation.viewport.MapViewportState
+import com.mapbox.maps.CameraOptions
+import com.mapbox.maps.MapView
+import com.mapbox.maps.Style
 
 class MainActivity : ComponentActivity() {
+    private lateinit var mapView: MapView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        MapboxOptions.accessToken = R.string.mapbox_access_token_public.toString()
-        setContent {
-//            Prototype_mapboxTheme {
-//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-//                    Greeting(
-//                            name = "Android",
-//                            modifier = Modifier.padding(innerPadding)
-//                    )
-//                }
-//            }
-            MapboxMap(
-                Modifier.fillMaxSize(),
-                mapViewportState = MapViewportState().apply {
-                    setCameraOptions {
-                        zoom(2.0)
-                        center(Point.fromLngLat(-98.0, 39.5))
-                        pitch(0.0)
-                        bearing(0.0)
-                    }
-                },
-            )
-        }
+//        enableEdgeToEdge()
+        MapboxOptions.accessToken = getString(R.string.mapbox_access_token_public)
+//        setContent {
+////            Prototype_mapboxTheme {
+////                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+////                    Greeting(
+////                            name = "Android",
+////                            modifier = Modifier.padding(innerPadding)
+////                    )
+////                }
+////            }
+//            MapboxMap(
+//                Modifier.fillMaxSize(),
+//                mapViewportState = MapViewportState().apply {
+//                    setCameraOptions {
+//                        zoom(2.0)
+//                        center(Point.fromLngLat(-98.0, 39.5))
+//                        pitch(0.0)
+//                        bearing(0.0)
+//                    }
+//                },
+//                style = { Style.MAPBOX_STREETS }
+//            )
+//        }
+
+        // Create a map programmatically and set the initial camera
+        mapView = MapView(this)
+        mapView.mapboxMap.setCamera(
+            CameraOptions.Builder()
+                .center(Point.fromLngLat(-98.0, 39.5))
+                .pitch(0.0)
+                .zoom(2.0)
+                .bearing(0.0)
+                .build()
+        )
+        mapView.mapboxMap.loadStyle(Style.MAPBOX_STREETS)
+        // Add the map view to the activity (you can also add it to other views as a child)
+        setContentView(mapView)
     }
 }
 
