83967870cfa5f4dd2e23fd7d8666645d7c8ba186
Garrett R Low <usernamegl@live.com>
2024-05-19 21:23:55 -0500

    Request user permissions and track location
    https://docs.mapbox.com/android/maps/guides/user-location/permissions/
    https://docs.mapbox.com/android/maps/guides/user-location/location-on-map/

--- prototype_mapbox/app/src/main/java/com/example/prototype_mapbox/MainActivity.kt
+++ prototype_mapbox/app/src/main/java/com/example/prototype_mapbox/MainActivity.kt
@@ -1,17 +1,54 @@
 package com.example.prototype_mapbox
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
+import com.mapbox.android.core.permissions.PermissionsListener
+import com.mapbox.android.core.permissions.PermissionsManager
 import com.mapbox.common.MapboxOptions
 import com.mapbox.geojson.Point
 import com.mapbox.maps.CameraOptions
 import com.mapbox.maps.MapView
 import com.mapbox.maps.Style
+import com.mapbox.maps.plugin.PuckBearing
+import com.mapbox.maps.plugin.locationcomponent.createDefault2DPuck
+import com.mapbox.maps.plugin.locationcomponent.location
+import com.mapbox.maps.plugin.viewport.viewport
 
 class MainActivity : ComponentActivity() {
     private lateinit var mapView: MapView
+    lateinit var permissionsManager: PermissionsManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        // https://docs.mapbox.com/android/maps/guides/user-location/permissions/
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+            // Permission sensitive logic called here, such as activating the Maps SDK's LocationComponent to show the device's location
+        } else {
+            permissionsManager = PermissionsManager(object : PermissionsListener {
+
+                override fun onExplanationNeeded(permissionsToExplain: List<String>) {
+                    val message = "You need to accept location permissions."
+                    val toast = Toast.makeText(
+                        applicationContext,
+                        message,
+                        Toast.LENGTH_SHORT
+                    )
+                    toast.show()
+                }
+
+                override fun onPermissionResult(granted: Boolean) {
+//                    activityRef.get()?.let {
+//                        if (granted) {
+//                            onMapReady()
+//                        } else {
+//                            it.finish()
+//                        }
+//                    }
+                }
+            })
+            permissionsManager.requestLocationPermissions(this)
+        }
+
         super.onCreate(savedInstanceState)
 //        enableEdgeToEdge()
         MapboxOptions.accessToken = getString(R.string.mapbox_access_token_public)
@@ -51,6 +88,26 @@ class MainActivity : ComponentActivity() {
         mapView.mapboxMap.loadStyle(Style.MAPBOX_STREETS)
         // Add the map view to the activity (you can also add it to other views as a child)
         setContentView(mapView)
+        with(mapView) {
+            location.locationPuck = createDefault2DPuck(withBearing = true)
+            location.enabled = true
+            location.puckBearing = PuckBearing.COURSE
+            viewport.transitionTo(
+                targetState = viewport.makeFollowPuckViewportState(),
+                transition = viewport.makeImmediateViewportTransition()
+            )
+        }
+
+    }
+
+    // https://docs.mapbox.com/android/maps/guides/user-location/permissions/
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 }
 
